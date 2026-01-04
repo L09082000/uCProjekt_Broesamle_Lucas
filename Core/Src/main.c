9b5599a45c3d343c51b858a6dc5374f6cf7d5d1c
Lucas <Lucas@DESKTOP-PJME6OJ>
2026-01-04 13:14:45 +0100

    V1

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -17,7 +17,7 @@
  */
 /* USER CODE END Header */
 /* Includes ------------------------------------------------------------------*/
-#include "main.h"
+#include "main.h" // Test
 #include "cmsis_os.h"
 
 /* Private includes ----------------------------------------------------------*/
